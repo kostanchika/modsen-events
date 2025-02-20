4d0b6bef932ac6b7dc8c8c949174d8e266869030
Konstantin Baranchuk <kos5anchik@gmail.com>
2025-02-20 20:46:20 +0300

    feat: add models for authentification

---
+++ EventsAPI/Models/AuthorizationModels.cs
@@ -0,0 +1,16 @@
+﻿namespace EventsAPI.Models
+{
+    public record class LoginModel(
+        string Login, 
+        string Password
+    );
+
+    public record RegisterModel(
+        string Login,
+        string Password,
+        string Name,
+        string LastName,
+        DateTime BirthDateTime,
+        string Email
+    );
+}
