4aaeed919150d50497ace4ec8fef13a176080e2d
Konstantin Baranchuk <kos5anchik@gmail.com>
2025-02-21 21:52:11 +0300

    feat: add models for creating events

---
+++ EventsAPI/Models/EventsModels.cs
@@ -0,0 +1,29 @@
+﻿namespace EventsAPI.Models
+{
+    public record class CreateEventModel(
+        string Name,
+        string Description,
+        DateTime EventDateTime,
+        string Location,
+        EventCategories Category,
+        int MaximumParticipants,
+        IFormFile? Image
+    );
+
+    public record class ChangeEventModel(
+        string Description,
+        DateTime EventDateTime,
+        string Location,
+        EventCategories Category,
+        int MaximumParticipants,
+        IFormFile? Image
+    );
+
+    public record class GetEventsModel(
+        string? Name,
+        DateTime? DateFrom,
+        DateTime? DateTo,
+        string? Location,
+        EventCategories? Category
+    );
+}
--- EventsAPI/Repository/EventRepository.cs
+++ EventsAPI/Repository/EventRepository.cs
@@ -7,6 +7,7 @@ namespace EventsAPI.Repository
     public interface IEventRepository : IRepository<Event>
     {
         new Task<Event?> GetByIdAsync(int id);
+        IEnumerable<Event> GetAllEventsWithFilters(GetEventsModel model);
     }
     public class EventRepository : Repository<Event>, IEventRepository
     {
@@ -14,6 +15,32 @@ namespace EventsAPI.Repository
         new public async Task<Event?> GetByIdAsync(int id)
         {
             return await _dbSet.Include(e => e.Participants).FirstOrDefaultAsync(e => e.Id == id);
+        }
+        public IEnumerable<Event> GetAllEventsWithFilters(GetEventsModel model)
+        {
+            var query = _dbSet.AsQueryable();
+
+            if (model.Name != null)
+            {
+                query = query.Where(e => e.Name.Contains(model.Name));
+            }
+
+            if (model.DateFrom.HasValue)
+            {
+                query = query.Where(e => e.EventDateTime >= model.DateFrom);
+            }
+
+            if (model.DateTo.HasValue)
+            {
+                query = query.Where(e => e.EventDateTime <= model.DateTo);
+            }
+
+            if (model.Category.HasValue && model.Category.Value != EventCategories.Unspecified)
+            {
+                query = query.Where(e => e.Category == model.Category);
+            }
+
+            return query.AsEnumerable();
         }
     }
 }
