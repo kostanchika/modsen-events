7f3e2d2f63d67140b8744152eaacce8868e96fa5
Konstantin Baranchuk <kos5anchik@gmail.com>
2025-02-28 21:33:24 +0300

    feat: reorginized EventTests

--- EventsAPI.Tests/EventTests.cs
+++ EventsAPI.Tests/EventTests.cs
@@ -1,4 +1,7 @@
-﻿using EventsAPI.BLL.Services;
+﻿using AutoMapper;
+using EventsAPI.BLL.DTO;
+using EventsAPI.BLL.Mappers;
+using EventsAPI.BLL.Services;
 using EventsAPI.DAL.Data;
 using EventsAPI.DAL.Entities;
 using EventsAPI.DAL.Repositories;
@@ -16,6 +19,7 @@ namespace EventsAPI.Tests
 
             List<User> users = [
                 new User { Name = "Konstantin", LastName = "Baranchuk", BirthDateTime = DateTime.Now.AddYears(-19), Email = "kos5anchik@gmail.com", Events = [], Login = "kostanchik", PasswordHash="_", RefreshToken = "_", RefreshTokenExpiresAt = DateTime.UtcNow.AddDays(7), Role = "Admin"  },
+                new User { Name = "Dmitry", LastName = "Kudlacky", BirthDateTime = DateTime.Now.AddYears(-20), Email = "dm1try@gmail.com", Events = [], Login = "dmitry", PasswordHash="_", RefreshToken = "_", RefreshTokenExpiresAt = DateTime.UtcNow.AddDays(7), Role = "User"  },
                 ];
             List<Event> events = [
                     new Event { Name = "Вечеринка у бассейна", EventDateTime = DateTime.UtcNow.AddDays(1), Location = "Бассейн у Влада", MaximumParticipants = 10, Participants = [], Category = EventCategories.Health, Description = "Поплаваем, отдохнём", ImagePath="/images/default.png" },
@@ -30,224 +34,157 @@ namespace EventsAPI.Tests
             return context;
         }
 
-        public static string Login => "kostanchik";
-
-        [Fact(DisplayName = "Пользователь должен зарегистрироваться на событие")]
-        public async Task RegisterOnEvent()
+        public static EventsService CreateService()
         {
             var db = CreateDatabase();
             var userRepository = new UserRepository(db);
             var eventRepository = new EventRepository(db);
 
-            var eventItem = await eventRepository.GetByIdAsync(1);
-
             var emailSender = new EmailSender(null);
 
-            var eventsService = new EventsService(
-                userRepository, 
-                eventRepository, 
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<EventDTOMapper>();
+                cfg.AddProfile<UserDTOMapper>();
+            });
+            var mapper = config.CreateMapper();
+
+            return new EventsService(
+                userRepository,
+                eventRepository,
                 null,
                 null,
                 emailSender,
                 null,
-                null
+                mapper
             );
+        }
 
-            if (!eventItem.Participants.Select(u => u.Login).Contains(Login))
+        public static string Login => "kostanchik";
+        public static string Login2 => "dmitry";
+
+        [Fact(DisplayName = "Пользователь должен зарегистрироваться на событие")]
+        public async Task RegisterOnEvent()
+        {
+            var eventsService = CreateService();
+
+            try
             {
-                if (eventItem.Participants.Count < eventItem.MaximumParticipants)
-                {
-                    if (eventItem.EventDateTime >= DateTime.UtcNow)
-                    {
-                        await eventsService.RegisterUserForEventAsync(eventItem.Id, Login);
-                    }
-                }
-            }
+                await eventsService.RegisterUserForEventAsync(1, Login);
+            } catch { }
 
-            eventItem = await eventRepository.GetByIdAsync(1);
 
-            Assert.Single(eventItem.Participants);
+            var eventItem = await eventsService.GetEventAsync(1);
+
+            Assert.Equal(1, eventItem.CurrentParticipants);
         }
 
         [Fact(DisplayName = "Пользователь должен зарегистрироваться на событие только один раз")]
         public async Task RegisterOnEventOnlyOneTime()
         {
-            var db = CreateDatabase();
-            var userRepository = new UserRepository(db);
-            var eventRepository = new EventRepository(db);
+            var eventsService = CreateService();
 
-            var eventItem = await eventRepository.GetByIdAsync(1);
-
-            var emailSender = new EmailSender(null);
-
-            var eventsService = new EventsService(
-                userRepository,
-                eventRepository,
-                null,
-                null,
-                emailSender,
-                null,
-                null
-            );
-
-            if (!eventItem.Participants.Select(u => u.Login).Contains(Login))
+            try
             {
-                if (eventItem.Participants.Count < eventItem.MaximumParticipants)
-                {
-                    if (eventItem.EventDateTime >= DateTime.UtcNow)
-                    {
-                        try
-                        {
-                            await eventsService.RegisterUserForEventAsync(eventItem.Id, Login);
-                        }
-                        catch { }
-                    }
-                }
-            }
-
-            eventItem = await eventRepository.GetByIdAsync(1);
-
-            if (!eventItem.Participants.Select(u => u.Login).Contains(Login))
+                await eventsService.RegisterUserForEventAsync(1, Login);
+            } catch { }
+            try
             {
-                if (eventItem.Participants.Count < eventItem.MaximumParticipants)
-                {
-                    if (eventItem.EventDateTime >= DateTime.UtcNow)
-                    {
-                        try
-                        {
-                            await eventsService.RegisterUserForEventAsync(eventItem.Id, Login);
-                        }
-                        catch { }
-                    }
-                }
-            }
+                await eventsService.RegisterUserForEventAsync(1, Login);
+            } catch { }
 
-            eventItem = await eventRepository.GetByIdAsync(1);
+            var eventItem = await eventsService.GetEventAsync(1);
 
-            Assert.Single(eventItem.Participants);
+            Assert.Equal(1, eventItem.CurrentParticipants);
         }
 
         [Fact(DisplayName = "Пользователь должен зарегистрироваться и отписаться от события")]
         public async Task RegisterAndUnregisterEvent()
         {
-            var db = CreateDatabase();
-            var userRepository = new UserRepository(db);
-            var eventRepository = new EventRepository(db);
+            var eventsService = CreateService();
+                       
+            await eventsService.RegisterUserForEventAsync(1, Login);
 
-            var eventItem = await eventRepository.GetByIdAsync(1);
+            await eventsService.UnregisterUserFromEventAsync(1, Login);
+            
+            var eventItem = await eventsService.GetEventAsync(1);
 
-            var emailSender = new EmailSender(null);
-
-            var eventsService = new EventsService(
-                userRepository,
-                eventRepository,
-                null,
-                null,
-                emailSender,
-                null,
-                null
-            );
-
-            if (!eventItem.Participants.Select(u => u.Login).Contains(Login))
-            {
-                if (eventItem.Participants.Count < eventItem.MaximumParticipants)
-                {
-                    if (eventItem.EventDateTime >= DateTime.UtcNow)
-                    {
-                        await eventsService.RegisterUserForEventAsync(eventItem.Id, Login);
-                    }
-                }
-            }
-
-            if (eventItem.Participants.Select(u => u.Login).Contains(Login))
-            {
-                if (eventItem.EventDateTime >= DateTime.UtcNow)
-                {
-                    await eventsService.UnregisterUserFromEventAsync(eventItem.Id, Login);
-                }
-            }
-
-            eventItem = await eventRepository.GetByIdAsync(1);
-
-            Assert.Empty(eventItem.Participants);
-        }
-
-        [Fact(DisplayName = "Список событий пользователя должен вернуть 1 событие")]
-        public async Task GetUserEvents()
-        {
-            var db = CreateDatabase();
-            var userRepository = new UserRepository(db);
-            var eventRepository = new EventRepository(db);
-
-            var eventItem = await eventRepository.GetByIdAsync(1);
-
-            var emailSender = new EmailSender(null);
-
-            var eventsService = new EventsService(
-                userRepository,
-                eventRepository,
-                null,
-                null,
-                emailSender,
-                null,
-                null
-            );
-
-            if (!eventItem.Participants.Select(u => u.Login).Contains(Login))
-            {
-                if (eventItem.Participants.Count < eventItem.MaximumParticipants)
-                {
-                    if (eventItem.EventDateTime >= DateTime.UtcNow)
-                    {
-                        await eventsService.RegisterUserForEventAsync(eventItem.Id, Login);
-                    }
-                }
-            }
-
-            var userEvents = await userRepository.GetByLoginIncludeEventsAsync(Login);
-
-            Assert.Single(userEvents.Events);
+            Assert.Equal(0, eventItem.CurrentParticipants);
         }
 
         [Fact(DisplayName = "Пользователь не должен подписаться на просроченное событие")]
         public async Task NotRegisterOnExpiredEvent()
         {
-            var db = CreateDatabase();
-            var userRepository = new UserRepository(db);
-            var eventRepository = new EventRepository(db);
+            var eventsService = CreateService();
 
-            var eventItem = await eventRepository.GetByIdAsync(2);
-
-            var emailSender = new EmailSender(null);
-
-            var eventsService = new EventsService(
-                userRepository,
-                eventRepository,
-                null,
-                null,
-                emailSender,
-                null,
-                null
-            );
-
-            if (!eventItem.Participants.Select(u => u.Login).Contains(Login))
+            try
             {
-                if (eventItem.Participants.Count < eventItem.MaximumParticipants)
-                {
-                    if (eventItem.EventDateTime >= DateTime.UtcNow)
-                    {
-                        try
-                        {
-                            await eventsService.RegisterUserForEventAsync(eventItem.Id, Login);
-                        }
-                        catch { }
-                    }
-                }
+                await eventsService.RegisterUserForEventAsync(2, Login);
             }
+            catch { }
 
-            eventItem = await eventRepository.GetByIdAsync(1);
+            var eventItem = await eventsService.GetEventAsync(1);
 
-            Assert.Empty(eventItem.Participants);
+            Assert.Equal(0, eventItem.CurrentParticipants);
+        }
+
+        [Fact(DisplayName = "Вывод информации о участниках события")]
+        public async Task GetEventParticipants()
+        {
+            var eventsService = CreateService();
+
+            await eventsService.RegisterUserForEventAsync(1, Login);
+            await eventsService.RegisterUserForEventAsync(1, Login2);
+
+            var participants = await eventsService.GetEventParticipantsAsync(1);
+
+            Assert.Equal(2, participants.Count());
+        }
+
+        [Fact(DisplayName = "Получение информации об участнике события")]
+        public async Task GetEventParticipant()
+        {
+            var eventsService = CreateService();
+
+            await eventsService.RegisterUserForEventAsync(1, Login);
+            await eventsService.RegisterUserForEventAsync(1, Login2);
+
+            var participant = await eventsService.GetEventParticipantAsync(1, 2);
+
+            Assert.Equal(Login2, participant.Login);
+        }
+
+        [Fact(DisplayName = "Получение информации о несуществующем участнике события")]
+        public async Task GetEventNullParticipant()
+        {
+            var eventsService = CreateService();
+
+            await eventsService.RegisterUserForEventAsync(1, Login);
+
+            UserDTO? participant = null;
+            try
+            {
+                participant = await eventsService.GetEventParticipantAsync(1, 2);
+            } catch { }
+
+            Assert.Null(participant);
+        }
+
+        [Fact(DisplayName = "Получение информации о участнике несуществующего события")]
+        public async Task GetNullEventParticipant()
+        {
+            var eventsService = CreateService();
+
+            await eventsService.RegisterUserForEventAsync(1, Login);
+
+            UserDTO? participant = null;
+            try
+            {
+                participant = await eventsService.GetEventParticipantAsync(3, 1);
+            }
+            catch { }
+
+            Assert.Null(participant);
         }
     }
 }
